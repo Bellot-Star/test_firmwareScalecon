a227139cedae17d73c4c9124f1b9b95896192527
Jin <cookingupgoodies@gmail.com>
2021-08-04 08:33:23 +1000

    second

--- pca10056/s140/ses/Output/ble_app_uart_pca10056_s140 Release/Obj/SEGGER_RTT_Syscalls_SES.d
+++
@@ -1,3 +0,0 @@
-Output/ble_app_uart_pca10056_s140\ Release/Obj/SEGGER_RTT_Syscalls_SES.o: \
- H:\CHJ\nRF5_16\external\segger_rtt\SEGGER_RTT_Syscalls_SES.c \
- ../config/sdk_config.h
--- pca10056/s140/ses/Output/ble_app_uart_pca10056_s140 Release/Obj/ses_startup_nrf_common.d
+++
@@ -1,2 +0,0 @@
-Output/ble_app_uart_pca10056_s140\ Release/Obj/ses_startup_nrf_common.o: \
- H:\CHJ\nRF5_16\modules\nrfx\mdk\ses_startup_nrf_common.s
